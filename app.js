fab695e0f3522e884267ea929d948d9778d33d3f
riko <enrico@gmail.com>
2022-02-07 21:56:27 +0700

    Security level 2 (Encrypt database with mongoose-encryption)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 app.use(express.static("public"));
@@ -11,10 +12,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+// AES Encrypt : https://www.npmjs.com/package/mongoose-encryption
+const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-};
+});
+
+const secret = "EnricoRiskiPutra";
+userSchema.plugin(encrypt, { secret: secret, encryptedField: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
